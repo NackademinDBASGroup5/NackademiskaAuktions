b738c202c3ccd2540ab8cb3defba64ff0987c57f
Simon Borgström <borgstrom.simon@gmail.com>
2016-02-19 00:24:09 +0100

    provisionreport added.

---
+++ NackademiskaAuktions/src/application/Provision.java
@@ -0,0 +1,26 @@
+package application;
+
+public class Provision {
+	private String manad;
+	private String provision;
+	public Provision(String manad, String provision) {
+		super();
+		this.manad = manad;
+		this.provision = provision;
+	}
+	public String getManad() {
+		return manad;
+	}
+	public void setManad(String manad) {
+		this.manad = manad;
+	}
+	public String getProvision() {
+		return provision;
+	}
+	public void setProvision(String provision) {
+		this.provision = provision;
+	}
+	
+	
+	
+}
