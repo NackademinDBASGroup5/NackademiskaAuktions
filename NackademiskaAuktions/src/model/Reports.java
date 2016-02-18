879093ce0a364dda8cc1900839aab8b9703d10e6
manssonskij <anton.mansson@gmail.com>
2016-02-19 00:41:13 +0100

    wtf twice

--- NackademiskaAuktions/src/model/Reports.java
+++ NackademiskaAuktions/src/model/Reports.java
@@ -2,38 +2,6 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class Reports {
-
-	
-	Connection connect = null;
-	PreparedStatement pstm = null;
-	ResultSet rset = null;
-	
-	//temporary
-	public Reports(){
-		try {
-			connect = DriverManager.getConnection("jdbc:mysql://localhost/auktion", "simon", "abc123");
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}	
-		
-	public void showProvisions() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-}
-package model;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
