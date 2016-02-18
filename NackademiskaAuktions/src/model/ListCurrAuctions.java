926f09684e3001e1a9d2e51cf33c77a424bdd5b7
thobiasp <thobias.pettersson@gmail.com>
2016-02-18 14:34:16 +0100

    fixed error

--- NackademiskaAuktions/src/model/ListCurrAuctions.java
+++ NackademiskaAuktions/src/model/ListCurrAuctions.java
@@ -34,7 +34,7 @@ public class ListCurrAuctions {
 	}
 	
 	
-	public ResultSet getAuctions(String from, String to){
+	public ResultSet getAuctionsIntervall(String from, String to){
 		
 		
 		try {
@@ -63,5 +63,8 @@ public class ListCurrAuctions {
 		return rs;
 	
 	}
+	
+	
+	
 
 }
