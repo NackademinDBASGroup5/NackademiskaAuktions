7e39f1d369547cc4b8483daf630f34fe609b812c
manssonskij <anton.mansson@gmail.com>
2016-02-19 17:47:32 +0100

    guitest

--- NackademiskaAuktions/src/model/RegisterAuktion.java
+++ NackademiskaAuktions/src/model/RegisterAuktion.java
@@ -26,7 +26,7 @@ public class RegisterAuktion {
 
 	public RegisterAuktion() {
 		try {
-			connect = DriverManager.getConnection("jdbc:mysql://localhost/auktion", "Simon", "abc123");
+			connect = DriverManager.getConnection("jdbc:mysql://localhost/auktion", "simon", "abc123");
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
